f65adc7c5d2d253cea11a588b2f6d5de670f57b7
PutkisDude <putkis@gmail.com>
2022-05-26 20:06:44 +0300

    added ListView to root url

--- ticket/views.py
+++ ticket/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from . import models
 
-# Create your views here.
+class TicketListView(ListView):
+	model = models.Ticket
